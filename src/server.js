1726c5a495e74fc609611ccae580707bab33de40
KennerEspinal <kennerespinalg@gmail.com>
2023-05-21 17:24:32 -0500

    WinnersAPI modified server.js

--- src/server.js
+++ src/server.js
@@ -20,8 +20,10 @@ app.get("/", (req, res) => {
 
 //mongodb connect
 const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 const start = async () => {
     try {
+        await ConnectDB(MONGODB_URI)
         app.listen(PORT, () => {
             console.log(`API is listening on port ${PORT}`);
             v1SwaggerDocs(app, PORT);
